2b6ae4c364757fa93cf57324e836854ae7e3abc9
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-13 17:56:52 +0200

    add github action

--- src/particle_mesh_coupling.jl
+++ src/particle_mesh_coupling.jl
@@ -2,6 +2,7 @@ using FastGaussQuadrature
 
 import GEMPIC: SplinePP
 import GEMPIC: uniform_bsplines_eval_basis
+import GEMPIC: horner_1d
 
 export ParticleMeshCoupling
 
@@ -105,42 +106,6 @@ end
 
 
 """
-    add_charge_pp!(rho_dofs, p, position, marker_charge)
-
-Add charge of one particle
-- `p`             : kernel smoother object
-- `position`      : Position of the particle
-- `marker_charge` : Particle weights time charge
-- `rho_dofs`      : Coefficient vector of the charge distribution
-"""
-function add_charge_pp!(
-    rho_dofs::Vector{Float64},
-    p::ParticleMeshCoupling,
-    position,
-    marker_charge,
-)
-
-    xi = (position[1] - p.domain[1]) / p.delta_x
-    index = floor(Int64, xi) + 1
-    xi = xi - (index - 1)
-    index = index - p.spline_degree
-
-    for i in eachindex(p.spline_val)
-
-        p.spline_val[i] = horner_1d(p.spline_degree, p.spline_pp.poly_coeffs, xi, i)
-
-    end
-
-    for i = 1:p.n_span
-        index1d = mod(index + i - 2, p.n_grid[1]) + 1
-        rho_dofs[index1d] =
-            rho_dofs[index1d] + (marker_charge * p.spline_val[i] * p.scaling)
-    end
-
-end
-
-
-"""
     add_charge!( rho, p, position, marker_charge) 
 
 Add charge of one particle
@@ -170,117 +135,11 @@ function add_charge!(
 
 end
 
-"""  
-    add_current_update_v_pp!( j_dofs, p, position_old, position_new, 
-                              marker_charge, qoverm, bfield_dofs, vi)
-
-Add current for one particle and update v 
-(according to `H_p1` part in Hamiltonian splitting)
-"""
-function add_current_update_v_pp!(
-    j_dofs::AbstractArray,
-    p::ParticleMeshCoupling,
-    position_old,
-    position_new,
-    marker_charge::Float64,
-    qoverm::Float64,
-    bfield_dofs::Vector{Float64},
-    vi::Float64,
-)
-
-    # Read out particle position and velocity
-    # Compute index_old, the index of the last DoF on the grid the particle
-    # contributes to, and r_old, its position (normalized to cell size one).
-
-    xi = (position_old[1] - p.domain[1]) / p.delta_x[1]
-    index_old = floor(Int64, xi)
-    r_old = xi - index_old
-
-    # Compute the new box index index_new and normalized position r_old.
-    xi = (position_new[1] - p.domain[1]) / p.delta_x[1]
-    index_new = floor(Int64, xi)
-    r_new = xi - index_new
-
-    if index_old == index_new
-
-        vi = update_jv_pp!(j_dofs, p, r_old, r_new, index_old, marker_charge, qoverm, vi)
-
-    elseif index_old < index_new
-
-        vi = update_jv_pp!(j_dofs, p, r_old, 1.0, index_old, marker_charge, qoverm, vi)
-
-        vi = update_jv_pp!(j_dofs, p, 0.0, r_new, index_new, marker_charge, qoverm, vi)
-
-        for ind = index_old+1:index_new-1
-            vi = update_jv_pp!(j_dofs, p, 0.0, 1.0, ind, marker_charge, qoverm, vi)
-        end
-
-    else
-
-        vi = update_jv_pp!(j_dofs, p, 1.0, r_new, index_new, marker_charge, qoverm, vi)
-
-        vi = update_jv_pp!(j_dofs, p, r_old, 0.0, index_old, marker_charge, qoverm, vi)
-
-        for ind = index_new+1:index_old-1
-            vi = update_jv_pp!(j_dofs, p, 1.0, 0.0, ind, marker_charge, qoverm, vi)
-        end
-
-    end
-
-
-end
-
-"""
-    update_jv_pp!( j_dofs, p, lower, upper, index, marker_charge, 
-                   qoverm, vi, bfield_dofs)
-
-Helper function for `add_current_update_v`.
-"""
-function update_jv_pp!(
-    j_dofs::AbstractArray,
-    p::ParticleMeshCoupling,
-    lower::Float64,
-    upper::Float64,
-    index::Int64,
-    marker_charge::Float64,
-    qoverm::Float64,
-    vi::Float64,
-)
-
-
-    n_cells = p.n_grid[1]
-
-    # Evaluation of the primitive integral at the lower 
-    # and upper bound of the gridcell
-
-    horner_primitive_1d(p.spline_val, p.spline_degree, p.spline_pp.poly_coeffs_fp, lower)
-    horner_primitive_1d(
-        p.spline_val_more,
-        p.spline_degree,
-        p.spline_pp.poly_coeffs_fp,
-        upper,
-    )
-
-    p.spline_val .= (p.spline_val_more .- p.spline_val) .* (p.delta_x[1])
-
-    ind = 1
-    for i_grid = index-p.spline_degree:index
-        i_mod = mod(i_grid, n_cells) + 1
-        j_dofs[i_mod] = j_dofs[i_mod] + (marker_charge * p.spline_val[ind] * p.scaling)
-        vi = vi
-        ind = ind + 1
-    end
-
-    vi
-
-end
-
-
 """
     add_current_update_v!( j_dofs, p, 
                            position_old, position_new, 
                            marker_charge, qoverm, 
-                           bfield_dofs, vi) 
+                           vi) 
 
 Add current for one particle and update v (according to ``H_{p1}``
 part in Hamiltonian splitting)
@@ -417,30 +276,6 @@ function update_jv!(
 
 end
 
-
-"""
-    evaluate_pp(p, position, field_dofs_pp)
-
-Evaluate field at `position` using horner scheme
-- `p` : Kernel smoother object 
-- `position` : Position of the particle
-- `field_dofs_pp` : Degrees of freedom in kernel representation.
-- `field_value` : Value(s) of the electric fields at given position
-
-"""
-function evaluate_pp(
-    p::ParticleMeshCoupling,
-    position::Float64,
-    field_dofs_pp::Array{Float64,2},
-)
-
-    xi = (position[1] - p.domain[1]) / p.delta_x[1]
-    index = floor(Int64, xi) + 1
-    xi = xi - (index - 1)
-
-    horner_1d(p.spline_degree, field_dofs_pp, xi, index)
-
-end
 
 """
     evaluate(p, position, field_dofs)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,3 @@ using Test
 using SpinGEMPIC
 
 include("test_mesh.jl")
-include("test_sampling.jl")
-include("test_particle_mesh_coupling_spline_1d.jl")
-include("test_hamiltonian_splitting.jl")
