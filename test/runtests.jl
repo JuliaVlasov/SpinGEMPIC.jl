7f0715cfcff447de46b543023dc8417d1910651b
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-13 16:54:17 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,6 @@ using Test
 using SpinGEMPIC
 
 include("test_mesh.jl")
-include("test_spline_pp.jl")
-include("test_maxwell_1d_fem.jl")
 include("test_sampling.jl")
 include("test_particle_mesh_coupling_spline_1d.jl")
 include("test_hamiltonian_splitting.jl")
-include("test_hamiltonian_splitting_boris.jl")
